09d76df2fdd1af0aad17664ef51647844365f3f6
Andrey Ageew <faceofdeviler@gmail.com>
2023-06-17 01:01:42 +0300

    add path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
 import pytest
 from src.item import Item
-
+import os
 
 @pytest.fixture
 def item():
     item = Item('Ноутбук', 1000.0, 5)
-    item2 = Item('Ноутбук', 1000.0, 5)
+    item2 = Item('Компьютер', 1000.0, 5)
     yield item, item2
+
+
+@pytest.fixture
+def path():
+    return os.path.abspath("src/items.csv")
