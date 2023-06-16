4b1595c3734ecea7041f454ec8a475b68039dfaa
Andrey Ageew <faceofdeviler@gmail.com>
2023-06-16 17:12:11 +0300

    add test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item(item):
+    item = item[0]
+    assert item.name == 'Ноутбук'
+    item.name = 'Планшет'
+    assert item.name == 'Планшет'
+    assert item.price == 1000.0
+    assert item.calculate_total_price() == 5000.0
+    item.pay_rate = 0.9
+    item.apply_discount()
+    assert item.price == 900.0
+    assert len(Item.all) == 2
